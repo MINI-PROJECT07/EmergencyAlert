92d229c9f5c645670034ebedc5fe507ea93f4d26
tanmay-8 <tanmayshingde8@gmail.com>
2024-03-28 09:19:01 +0530

    separated backend

--- Backend/src/Routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-
-router.use("/user",require("./user"));
-router.use("/hospital",require("./hospital"));
-
-module.exports = router
